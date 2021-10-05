bd8f02dcc35ecb0cea30145772ac244d16bcbc61
Bob McAleavey <bobmcaleavey@gmail.com>
2021-10-05 13:26:34 -0400

    add favicons

---
+++ challenges/gradie-sign-up/build/internal/pages/terms-and-conditions.svelte-60b70a7f.js
@@ -0,0 +1,1 @@
+import{S as n,i as e,s as t}from"../chunks/vendor-25855a91.js";class l extends n{constructor(s){super();e(this,s,null,null,t,{})}}export{l as default};
--- challenges/gradie-sign-up/src/routes/__layout.svelte
+++ challenges/gradie-sign-up/src/routes/__layout.svelte
@@ -2,4 +2,10 @@
   import '../app.css';
 </script>
 
+<svelte:head>
+  <link rel="icon" type="image/svg+xml" href="./icon.svg" />
+  <link rel="alternate icon" href="./icon-16x16.png" />
+  <link rel="apple-touch-icon" href="./icon-apple-touch.png" />
+</svelte:head>
+
 <slot />
